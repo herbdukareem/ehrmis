00acbd64b4b8e5035aa8f0bbf104d41eaeba42ad
Abdulkarim <herbdukareemh@gmail.com>
2026-06-18 18:39:03 +0100

    fix token issues

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -7,6 +7,7 @@ use Illuminate\Foundation\Configuration\Middleware;
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
+        api: __DIR__.'/../routes/api.php',
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
     )
@@ -14,9 +15,15 @@ return Application::configure(basePath: dirname(__DIR__))
         __DIR__.'/../app/Console/Commands',
     ])
     ->withMiddleware(function (Middleware $middleware) {
+        $middleware->statefulApi();
+
         $middleware->web(append: [
             \App\Http\Middleware\ResolveDomainContext::class,
             \Illuminate\Http\Middleware\AddLinkHeadersForPreloadedAssets::class,
+        ]);
+
+        $middleware->api(append: [
+            \App\Http\Middleware\ResolveDomainContext::class,
         ]);
 
         $middleware->alias([
